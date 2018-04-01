85a4ce5ca311fb70177985c66319e251c77d9a8b
Revant Nandgaonkar <revant.one@gmail.com>
2018-04-01 18:47:03 +0530

    Fix Listing User

--- client/index.js
+++ client/index.js
@@ -5,7 +5,7 @@ module.exports = {
     start() {
         frappe.desk.menu.addItem('OAuthClient', '#list/OAuthClient');
         frappe.desk.menu.addItem('Session', '#list/Session');
-        frappe.desk.menu.addItem('OAuthClient', '#list/User');
+        frappe.desk.menu.addItem('User', '#list/User');
         
         frappe.router.default = '#list/Session';
         frappe.router.show(window.location.hash);
